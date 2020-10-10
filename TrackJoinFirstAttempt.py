ac88229efae8262386a7832092d88edaea9872f1
JereHu <34283813+JereHu@users.noreply.github.com>
2020-10-10 16:37:55 +0200

    Update TrackJoinFirstAttempt.py

--- TrackJoinFirstAttempt.py
+++ TrackJoinFirstAttempt.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import csv
+from collections import defaultdict
 
 TableR = pd.read_csv("TableR.csv")
 TableS = pd.read_csv("TableS.csv")
@@ -10,7 +11,35 @@ TableOut = open('TableOut.csv', 'w', newline='')
 for person in TableR["Name"]:
     TableT[person] = TableR[TableR["Name"] == person]
 
-print(TableT)
+
+
+
+def hashJoin(table1, index1, table2, index2):
+    h = defaultdict(list)
+    # hash phase
+    for s in table1:
+        h[s[index1]].append(s)
+    # join phase
+    return [(s, r) for r in table2 for s in h[r[index2]]]
+ 
+
+ 
+table1 = [(27, "Jonah"),
+          (18, "Alan"),
+          (28, "Glory"),
+          (18, "Popeye"),
+          (28, "Alan")]
+table2 = [("Jonah", "Whales"),
+          ("Jonah", "Spiders"),
+          ("Alan", "Ghosts"),
+          ("Alan", "Zombies"),
+          ("Glory", "Buffy")]
+
+
+ 
+'''for row in hashJoin(table1, 1, table2, 0):
+    print(row)
+    '''
 
 
 '''
